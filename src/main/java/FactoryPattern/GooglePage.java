55758d4509e5466b5dbc8cb08965f11166993666
Monil Joshi <monil.joshi@qualitia.local>
2020-10-10 01:36:26 +0530

    Added package for Factoy Pattern | Create abstract class GooglePage which contains three abstract method

---
+++ src/main/java/FactoryPattern/GooglePage.java
@@ -0,0 +1,9 @@
+package FactoryPattern;
+
+public abstract class GooglePage {
+
+    public abstract void launchSite();
+    public abstract void search(String userInput);
+    public abstract int getResultsCount();
+
+}
