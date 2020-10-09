5f5d6b9c843d3b1fce05598f05b152bfa962f1c2
Monil Joshi <monil.joshi@qualitia.local>
2020-10-09 16:57:02 +0530

    Added some more stpes in Google Search Test | Added code for Enter string, Select suggestion using Index, Click on News, print the render stats

--- src/test/java/SingleResponsibilityPrinciple/GoogleSearchTest.java
+++ src/test/java/SingleResponsibilityPrinciple/GoogleSearchTest.java
@@ -1,9 +1,12 @@
 package SingleResponsibilityPrinciple;
 
 import BaseTestPackage.BaseTest;
+import com.google.common.util.concurrent.Uninterruptibles;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class GoogleSearchTest extends BaseTest {
     private GoogleMainPage googleMainPage;
@@ -17,7 +20,26 @@ public class GoogleSearchTest extends BaseTest {
 
     @Test
     public void searchTestWorkFlow(){
+        String userInput = "Selenium WebDriver";
+        int index = 3;
+
         googleMainPage.goToGoogleHomePage();
         Assert.assertTrue(googleMainPage.getSearchBox().isDisplay());
+
+        googleMainPage.getSearchBox().enterAString(userInput);
+        Assert.assertTrue(googleMainPage.getSearchSuggestions().isDisplay());
+
+        googleMainPage.getSearchSuggestions().clickOnSuggestionByIndex(index);
+        Assert.assertTrue(googleResultPage.getSearchSuggestionsComponent().isDisplay());
+
+        googleResultPage.getSearchBoxComponent().enterAString(userInput);
+        Assert.assertTrue(googleResultPage.getSearchSuggestionsComponent().isDisplay());
+
+        googleResultPage.getSearchSuggestionsComponent().clickOnSuggestionByIndex(index);
+
+        googleResultPage.getSearchPageMenuComponent().clickOnNews();
+
+        System.out.println(googleResultPage.getResultStatComponent().getResultStats());
+
     }
 }
