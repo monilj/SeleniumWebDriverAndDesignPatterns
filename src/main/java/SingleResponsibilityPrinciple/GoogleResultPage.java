5aff706fdcd99bf29e47a5e479c30dc0baec68ba
Monil Joshi <monil.joshi@qualitia.local>
2020-10-09 14:17:56 +0530

    Single Responisbility Principle | Added a page class which returns objects of the components on the Search Result page

---
+++ src/main/java/SingleResponsibilityPrinciple/GoogleResultPage.java
@@ -0,0 +1,35 @@
+package SingleResponsibilityPrinciple;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class GoogleResultPage {
+
+    private SearchBoxComponent searchBoxComponent;
+    private SearchSuggestionsComponent searchSuggestionsComponent;
+    private SearchPageMenuComponent searchPageMenuComponent;
+    private ResultStatComponent resultStatComponent;
+
+    public GoogleResultPage(final WebDriver driver){
+        this.searchBoxComponent = PageFactory.initElements(driver, SearchBoxComponent.class);
+        this.searchSuggestionsComponent = PageFactory.initElements(driver, SearchSuggestionsComponent.class);
+        this.searchPageMenuComponent = PageFactory.initElements(driver, SearchPageMenuComponent.class);
+        this.resultStatComponent = PageFactory.initElements(driver, ResultStatComponent.class);
+    }
+
+    public SearchBoxComponent getSearchBoxComponent() {
+        return searchBoxComponent;
+    }
+
+    public SearchSuggestionsComponent getSearchSuggestionsComponent() {
+        return searchSuggestionsComponent;
+    }
+
+    public SearchPageMenuComponent getSearchPageMenuComponent() {
+        return searchPageMenuComponent;
+    }
+
+    public ResultStatComponent getResultStatComponent() {
+        return resultStatComponent;
+    }
+}
