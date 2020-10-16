d3ba3ec4c543e826ff5654d7cd4c50ff57260cb5
Monil Joshi <monil.joshi@qualitia.local>
2020-10-16 13:17:14 +0530

    Factory Design Pattern | Implement Factory class for Google

---
+++ src/main/java/FactoryPattern/GoogleFactory.java
@@ -0,0 +1,24 @@
+package FactoryPattern;
+
+import org.openqa.selenium.WebDriver;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+
+public class GoogleFactory {
+    private static final Function<WebDriver,GooglePage> ENG = (driver -> new GoogleInEnglish(driver));
+    private static final Function<WebDriver,GooglePage> FR = (driver -> new GoogleInFrench(driver));
+    private static final Function<WebDriver,GooglePage> SA = (driver -> new GoogleInArabic(driver));
+    private static final Map<String, Function<WebDriver,GooglePage>> MAP = new HashMap<>();
+
+    static {
+        MAP.put("ENG", ENG);
+        MAP.put("FR", FR);
+        MAP.put("SA", SA);
+    }
+    public static GooglePage get(String language, WebDriver driver){
+        return MAP.get(language).apply(driver);
+    }
+}
