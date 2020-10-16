f85ff1e4a1b2d3097e01b641bdfc67854a118cd9
Monil Joshi <monil.joshi@qualitia.local>
2020-10-16 14:35:50 +0530

    Factory Design Pattern | Added a test class for Google Search Test | Implemeneted test

---
+++ src/test/java/FactoryDesignPattern/GoogleSearchTest.java
@@ -0,0 +1,32 @@
+package FactoryDesignPattern;
+
+import BaseTestPackage.BaseTest;
+import FactoryPattern.GoogleFactory;
+import FactoryPattern.GooglePage;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class GoogleSearchTest extends BaseTest {
+
+    private GooglePage googlePage;
+
+    @Test(dataProvider = "getData")
+    public void searchTest(String language, String userInput ){
+        this.googlePage = GoogleFactory.get(language,this.driver);
+        this.googlePage.launchSite();
+        this.googlePage.search(userInput);
+        int resultsCount= this.googlePage.getResultsCount();
+        System.out.println(resultsCount);
+    }
+
+    @DataProvider
+    public Object[][] getData(){
+        return new Object[][]{
+                {"ENG","Selenium"},
+                {"FR","Docker"},
+                {"SA","TestNg"}
+
+
+        };
+    }
+}
