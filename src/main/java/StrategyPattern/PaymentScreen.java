8b8410d93585ee49c9d719b65af6d6920de66c6a
Monil Joshi <monil.joshi@qualitia.local>
2020-10-16 19:51:35 +0530

    Strategy Design Pattern | Added simple class with constructor for Payment Screen

---
+++ src/main/java/StrategyPattern/PaymentScreen.java
@@ -0,0 +1,12 @@
+package StrategyPattern;
+
+import org.openqa.selenium.WebDriver;
+
+public class PaymentScreen {
+    private WebDriver driver;
+
+    public PaymentScreen(final WebDriver driver){
+        this.driver = driver;
+    }
+
+}
