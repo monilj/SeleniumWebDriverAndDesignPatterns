307a1d153d4dcf76c6b462d368e6e5b4b068eef4
Monil Joshi <monil.joshi@qualitia.local>
2020-10-19 23:28:03 +0530

    Strategy design Pattern | Connecting All the componenets in Page object class PaymentScreen

--- src/main/java/StrategyPattern/PaymentScreen.java
+++ src/main/java/StrategyPattern/PaymentScreen.java
@@ -1,12 +1,35 @@
 package StrategyPattern;
 
+import com.sun.org.apache.xpath.internal.operations.Or;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
 public class PaymentScreen {
     private WebDriver driver;
+    private UserInformation userInformation;
+    private Order order;
+    private PaymentOption paymentOption;
 
     public PaymentScreen(final WebDriver driver){
         this.driver = driver;
+        this.userInformation = PageFactory.initElements(driver, UserInformation.class);
+        this.order = PageFactory.initElements(driver, Order.class);
     }
 
+    public UserInformation getUserInformation() {
+        return userInformation;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setPaymentOption(PaymentOption paymentOption) {
+        this.paymentOption = paymentOption;
+        PageFactory.initElements(driver,this.paymentOption);
+    }
+
+    public PaymentOption getPaymentOption() {
+        return paymentOption;
+    }
 }
