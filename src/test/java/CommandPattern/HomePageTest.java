546012c4ae2d50c38a08e425fae570a11e792aff
Monil Joshi <monil.joshi@qualitia.local>
2020-10-21 18:29:30 +0530

    Command Design Patter | Crete a test HomePagetest under Package CommandPattern

---
+++ src/test/java/CommandPattern/HomePageTest.java
@@ -0,0 +1,25 @@
+package CommandPattern;
+
+import BaseTestPackage.BaseTest;
+import org.apache.commons.lang3.builder.ToStringExclude;
+import org.junit.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class HomePageTest extends BaseTest {
+    private HomePage homePage;
+
+    @BeforeTest
+    private void setHomePage() {
+        this.homePage = new HomePage(driver);
+    }
+
+    @Test
+    public void homePageTest() {
+        this.homePage.goToSite();
+        this.homePage.getElementValidators()
+                .stream()
+                .map(ev -> ev.validate())
+                .forEach(b -> Assert.assertTrue(b));
+    }
+}
