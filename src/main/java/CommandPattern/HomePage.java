3b0eb41f9608bfcd273a6c21dc473ef2a9d5e8af
Monil Joshi <monil.joshi@qualitia.local>
2020-10-21 11:56:52 +0530

    Command Design Patter | Added web elements with  locators for buttons, Notifications and dismissal Alerts  inside HomePage

--- src/main/java/CommandPattern/HomePage.java
+++ src/main/java/CommandPattern/HomePage.java
@@ -11,6 +11,47 @@ import java.util.List;
 public class HomePage {
     private WebDriver driver;
 
+    //buttons
+    @FindBy(css = ".tst1.btn.btn-info")
+    private WebElement infoButton;
+
+    @FindBy(css = ".tst1.btn.btn-warning")
+    private WebElement warningButton;
+
+    @FindBy(css = ".tst1.btn.btn-success")
+    private WebElement successButton;
+
+    @FindBy(css = ".tst1.btn.btn-danger")
+    private WebElement dangerButton;
+
+    //notifications
+    @FindBy(css = "div.jq-icon-info")
+    private WebElement infoAlert;
+
+    @FindBy(css = "div.jq-icon-warning")
+    private WebElement warnAlert;
+
+    @FindBy(css = "div.jq-icon-success")
+    private WebElement successAlert;
+
+    @FindBy(css = "div.jq-icon-error")
+    private WebElement dangerAlert;
+
+    //dismissal Alerts
+    @FindBy(css = "div.alert-info")
+    private WebElement dismissInfoAlert;
+
+    @FindBy(css = "div.alert-success")
+    private WebElement dismissSuccessAlert;
+
+    @FindBy(css = "div.alert-danger")
+    private WebElement dismissDangerAlert;
+
+    @FindBy(css = "div.alert-warning")
+    private WebElement dismissWarnAlert;
+
+
+
     public HomePage(final WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
