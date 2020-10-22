903251eddfcce359e25c49075db9b08e256393ab
Monil Joshi <monil.joshi@qualitia.local>
2020-10-22 23:25:12 +0530

    Template Design Pattern | Implemented eBay Shopping class

---
+++ src/main/java/TemplatePattern/eBayShopping.java
@@ -0,0 +1,58 @@
+package TemplatePattern;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class eBayShopping extends ShoppingTemplate{
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private String product;
+
+    @FindBy(id = "gh-ac")
+    private WebElement searchBox;
+
+    @FindBy(id = "gh-btn")
+    private WebElement searchBtn;
+
+    @FindBy(css = "div.s-item__info a")
+    private WebElement item;
+
+    @FindBy(css = ".s-item__price")
+    private WebElement price;
+
+    public eBayShopping(final WebDriver driver,String product)
+    {
+        this.driver = driver;
+        this.product = product;
+        this.wait = new WebDriverWait(driver,15);
+        PageFactory.initElements(driver,this);
+    }
+
+    @Override
+    public void launchSite() {
+        this.driver.get("https://in.ebay.com/");
+
+    }
+
+    @Override
+    public void searchForProduct() {
+        this.searchBox.sendKeys(this.product);
+        this.searchBtn.click();
+    }
+
+    @Override
+    public void selectProduct() {
+        wait.until((driver1 -> item.isDisplayed()));
+        item.click();
+    }
+
+    @Override
+    public void buy() {
+        wait.until((driver1 -> price.isDisplayed()));
+        System.out.println(this.price.getText());
+    }
+}
