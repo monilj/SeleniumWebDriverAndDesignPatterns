c80751d4e1ab638298cc2d952c2c93f508ec3e96
Monil Joshi <monil.joshi@qualitia.local>
2020-10-23 23:26:11 +0530

    Proxy Design Pattern | Implemented OrderComponenetProxy

--- src/main/java/Proxy/OrderComponentProxy.java
+++ src/main/java/Proxy/OrderComponentProxy.java
@@ -1,8 +1,33 @@
 package Proxy;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 public class OrderComponentProxy implements OrderComponent  {
+    private static final List<String> EXCLUDED = Arrays.asList(
+      "PROD", "STAGING"
+    );
+    private OrderComponent orderComponent;
+
+    public OrderComponentProxy(final WebDriver driver){
+        String currentEnv = System.getProperty("env"); //return DEV, QA, PROD
+        if(!EXCLUDED.contains(currentEnv)) //for PROD it will return True otherwise false
+            this.orderComponent = new OrderComponentReal(driver);
+
+    }
+
     @Override
     public String placeOrder() {
-        return null;
+        if(Objects.nonNull(this.orderComponent))
+            {
+                return this.orderComponent.placeOrder();
+            }
+        else{
+            return "Skipped";
+        }
     }
 }
