49c18f7a0345d91c4ced2cd903683c8c5073e275
Monil Joshi <monil.joshi@qualitia.local>
2020-10-23 23:04:17 +0530

    Proxy Design Pattern | Create an interface for OrderComponenet | Created 2 class OrderComponentReal and OrderComponentProxy | Implemented OrderComponenetReal

---
+++ src/main/java/Proxy/OrderComponent.java
@@ -0,0 +1,5 @@
+package Proxy;
+
+public interface OrderComponent {
+    String placeOrder();
+}
---
+++ src/main/java/Proxy/OrderComponentProxy.java
@@ -0,0 +1,8 @@
+package Proxy;
+
+public class OrderComponentProxy implements OrderComponent  {
+    @Override
+    public String placeOrder() {
+        return null;
+    }
+}
