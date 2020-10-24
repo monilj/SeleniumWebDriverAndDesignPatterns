77726b6868248e69cbd482346a8b3a9bd8b3e74a
Monil Joshi <monil.joshi@qualitia.local>
2020-10-24 23:34:36 +0530

    Execute Around Method Design Pattern | Refactored Framt Test with the help of lmbda multiline feature

--- src/test/java/ExecuteAroundMethodDesignPattern/FrameTest.java
+++ src/test/java/ExecuteAroundMethodDesignPattern/FrameTest.java
@@ -15,8 +15,27 @@ public class FrameTest extends BaseTest {
     @Test
     public void frameTest(){
         this.mainPage.goToSite();
-       this.mainPage.onFrameA(a -> a.setFirstName("fn1"));
-        this.mainPage.onFrameA(a -> a.setLastName("ln1"));
+       this.mainPage.onFrameA(a ->
+       {
+           a.setFirstName("fn1");
+           a.setLastName("ln1");
+           a.setAddress("Nigdi");
+           a.setMessage("I am awesome");
+       });
+        this.mainPage.onFrameB(b ->
+        {
+            b.setFirstName("fn2");
+            b.setLastName("ln2");
+            b.setAddress("Akurdi");
+            b.setMessage("I am good");
+        });
+        this.mainPage.onFrameC(c ->
+        {
+            c.setFirstName("fn1");
+            c.setLastName("ln1");
+            c.setAddress("Nigdi");
+            c.setMessage("I am awesome");
+        });
     }
 }
 
