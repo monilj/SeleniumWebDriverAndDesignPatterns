37eaa50ee8b050fa7280c3d7ba05253b4e1d981f
Monil Joshi <monil.joshi@qualitia.local>
2020-10-24 19:36:20 +0530

    Execute Around Method Design Pattern | Implement Main Page and created object of frame classes

---
+++ src/main/java/ExecuteAroundMethodDesignPattern/MainPage.java
@@ -0,0 +1,37 @@
+package ExecuteAroundMethodDesignPattern;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import sun.applet.Main;
+
+public class MainPage {
+    public WebDriver driver;
+
+    @FindBy(id = "a")
+    private WebElement frameA;
+
+    @FindBy(id = "b")
+    private WebElement frameB;
+
+    @FindBy(id = "c")
+    private WebElement frameC;
+
+    private FrameA frameObjectA;
+    private FrameB frameObjectB;
+    private FrameC frameObjectC;
+
+
+    public MainPage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+        this.frameObjectA = PageFactory.initElements(driver,FrameA.class);
+        this.frameObjectB = PageFactory.initElements(driver,FrameB.class);
+        this.frameObjectC = PageFactory.initElements(driver,FrameC.class);
+    }
+
+    public void goToSite(){
+        this.driver.get("https://vins-udemy.s3.amazonaws.com/ds/main.html");
+    }
+}
